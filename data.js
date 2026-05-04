a30b941066a9bfebd687026bfc0c68078ac8ecb2
findyouruni <cherie.balbina.gao@sekolah.pahoa.sch.id>
2026-05-04 07:39:52 +0700

    Create data.js

---
+++ data.js
@@ -0,0 +1,14 @@
+const universities = [
+  { name: "MIT", country: "USA", rank: 1, minGPA: 3.7, avgGPA: 3.9 },
+  { name: "Harvard", country: "USA", rank: 4, minGPA: 3.7, avgGPA: 3.9 },
+  { name: "Stanford", country: "USA", rank: 6, minGPA: 3.7, avgGPA: 3.9 },
+  { name: "Oxford", country: "UK", rank: 3, minGPA: 3.7, avgGPA: 3.9 },
+  { name: "Cambridge", country: "UK", rank: 5, minGPA: 3.7, avgGPA: 3.9 },
+
+  { name: "UCLA", country: "USA", rank: 30, minGPA: 3.5, avgGPA: 3.8 },
+  { name: "Toronto", country: "Canada", rank: 26, minGPA: 3.5, avgGPA: 3.8 },
+  { name: "NUS", country: "Singapore", rank: 8, minGPA: 3.7, avgGPA: 3.9 },
+
+  { name: "Melbourne", country: "Australia", rank: 13, minGPA: 3.5, avgGPA: 3.8 },
+  { name: "Manchester", country: "UK", rank: 32, minGPA: 3.3, avgGPA: 3.7 }
+];
