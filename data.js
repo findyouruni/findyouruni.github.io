a6d5750db9ee3c2368924778b19b3ae86be7e3d2
findyouruni <cherie.balbina.gao@sekolah.pahoa.sch.id>
2026-05-17 22:38:18 +0700

    Update data.js

--- data.js
+++ data.js
@@ -2,86 +2,107 @@ const universities = [
 
   {
     name: "MIT",
+    rank: 1,
     website: "https://www.mit.edu",
-    logo: "https://upload.wikimedia.org/wikipedia/commons/0/0c/MIT_logo.svg",
-    generalRank: 1
+    logo: "https://upload.wikimedia.org/wikipedia/commons/0/0c/MIT_logo.svg"
   },
 
   {
     name: "Imperial College London",
+    rank: 2,
     website: "https://www.imperial.ac.uk",
-    logo: "https://upload.wikimedia.org/wikipedia/commons/c/c5/Imperial_College_London_new_logo.svg",
-    generalRank: 2
+    logo: "https://upload.wikimedia.org/wikipedia/commons/2/28/Imperial_College_London_new_logo.svg"
   },
 
   {
     name: "Stanford University",
+    rank: 3,
     website: "https://www.stanford.edu",
-    logo: "https://upload.wikimedia.org/wikipedia/en/b/b7/Stanford_University_seal_2003.svg",
-    generalRank: 3
+    logo: "https://upload.wikimedia.org/wikipedia/en/b/b7/Stanford_University_seal_2003.svg"
   },
 
   {
     name: "University of Oxford",
+    rank: 4,
     website: "https://www.ox.ac.uk",
-    logo: "https://upload.wikimedia.org/wikipedia/en/d/d5/Oxford_University_Circlet.svg",
-    generalRank: 4
+    logo: "https://upload.wikimedia.org/wikipedia/en/d/d5/Oxford_University_Circlet.svg"
   },
 
   {
     name: "Harvard University",
+    rank: 5,
     website: "https://www.harvard.edu",
-    logo: "https://upload.wikimedia.org/wikipedia/en/2/29/Harvard_shield_wreath.svg",
-    generalRank: 5
+    logo: "https://upload.wikimedia.org/wikipedia/en/2/29/Harvard_shield_wreath.svg"
   },
 
   {
     name: "University of Cambridge",
+    rank: 6,
     website: "https://www.cam.ac.uk",
-    logo: "https://upload.wikimedia.org/wikipedia/en/8/8a/University_of_Cambridge_coat_of_arms.svg",
-    generalRank: 6
+    logo: "https://upload.wikimedia.org/wikipedia/en/8/8a/University_of_Cambridge_coat_of_arms.svg"
   },
 
   {
     name: "ETH Zurich",
+    rank: 7,
     website: "https://ethz.ch",
-    logo: "https://upload.wikimedia.org/wikipedia/commons/f/f2/ETH_Zurich_Logo.svg",
-    generalRank: 7
+    logo: "https://upload.wikimedia.org/wikipedia/commons/f/f2/ETH_Zurich_Logo.svg"
   },
 
   {
     name: "National University of Singapore",
+    rank: 8,
     website: "https://www.nus.edu.sg",
-    logo: "https://upload.wikimedia.org/wikipedia/en/9/9a/NUS_coat_of_arms.svg",
-    generalRank: 8
+    logo: "https://upload.wikimedia.org/wikipedia/en/9/9a/NUS_coat_of_arms.svg"
   },
 
   {
     name: "UCL",
+    rank: 9,
     website: "https://www.ucl.ac.uk",
-    logo: "https://upload.wikimedia.org/wikipedia/commons/0/04/UCL_logo.svg",
-    generalRank: 9
+    logo: "https://upload.wikimedia.org/wikipedia/commons/0/04/UCL_logo.svg"
   },
 
   {
     name: "California Institute of Technology",
+    rank: 10,
     website: "https://www.caltech.edu",
-    logo: "https://upload.wikimedia.org/wikipedia/commons/7/75/Caltech_Logo.svg",
-    generalRank: 10
+    logo: "https://upload.wikimedia.org/wikipedia/commons/7/75/Caltech_Logo.svg"
   },
 
   {
     name: "University of Pennsylvania",
+    rank: 11,
     website: "https://www.upenn.edu",
-    logo: "https://upload.wikimedia.org/wikipedia/en/9/9a/University_of_Pennsylvania_Shield.svg",
-    generalRank: 11
+    logo: "https://upload.wikimedia.org/wikipedia/en/9/9a/University_of_Pennsylvania_Shield.svg"
   },
 
   {
     name: "University of California, Berkeley",
+    rank: 12,
     website: "https://www.berkeley.edu",
-    logo: "https://upload.wikimedia.org/wikipedia/commons/1/1f/UC_Berkeley_Seal.svg",
-    generalRank: 12
+    logo: "https://upload.wikimedia.org/wikipedia/commons/1/1f/UC_Berkeley_Seal.svg"
+  },
+
+  {
+    name: "University of Melbourne",
+    rank: 13,
+    website: "https://www.unimelb.edu.au",
+    logo: "https://upload.wikimedia.org/wikipedia/en/b/b4/University_of_Melbourne_coat_of_arms.svg"
+  },
+
+  {
+    name: "Peking University",
+    rank: 14,
+    website: "https://english.pku.edu.cn",
+    logo: "https://upload.wikimedia.org/wikipedia/en/1/17/Peking_University_logo.svg"
+  },
+
+  {
+    name: "Nanyang Technological University",
+    rank: 15,
+    website: "https://www.ntu.edu.sg",
+    logo: "https://upload.wikimedia.org/wikipedia/en/4/48/Nanyang_Technological_University.svg"
   }
 
 ];
