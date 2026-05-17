4910457a1469ec43241d828a0cb9818c9ba5659b
findyouruni <cherie.balbina.gao@sekolah.pahoa.sch.id>
2026-05-17 22:23:13 +0700

    Update data.js

--- data.js
+++ data.js
@@ -1,62 +1,87 @@
 const universities = [
+
   {
-    name: "Massachusetts Institute of Technology",
-    logo: "https://www.topuniversities.com/sites/default/files/profiles/logos/massachusetts-institute-of-technology-mit_410_large.jpg",
+    name: "MIT",
     website: "https://www.mit.edu",
+    logo: "https://upload.wikimedia.org/wikipedia/commons/0/0c/MIT_logo.svg",
     generalRank: 1
   },
+
   {
     name: "Imperial College London",
-    logo: "https://www.topuniversities.com/sites/default/files/profiles/logos/240430033452pm869301QS-Imperial-Logo-white-text-blue-background-200x200.jpg",
-    website: "https://www.imperial.ac.uk/",
+    website: "https://www.imperial.ac.uk",
+    logo: "https://upload.wikimedia.org/wikipedia/commons/c/c5/Imperial_College_London_new_logo.svg",
     generalRank: 2
   },
+
   {
     name: "Stanford University",
-    logo: "https://upload.wikimedia.org/wikipedia/en/b/b7/Stanford_University_seal_2003.svg",
     website: "https://www.stanford.edu",
+    logo: "https://upload.wikimedia.org/wikipedia/en/b/b7/Stanford_University_seal_2003.svg",
     generalRank: 3
   },
+
   {
     name: "University of Oxford",
-    logo: "https://upload.wikimedia.org/wikipedia/en/d/d5/Oxford_University_Circlet.svg",
     website: "https://www.ox.ac.uk",
+    logo: "https://upload.wikimedia.org/wikipedia/en/d/d5/Oxford_University_Circlet.svg",
     generalRank: 4
   },
+
   {
     name: "Harvard University",
-    logo: "https://upload.wikimedia.org/wikipedia/en/2/29/Harvard_shield_wreath.svg",
     website: "https://www.harvard.edu",
+    logo: "https://upload.wikimedia.org/wikipedia/en/2/29/Harvard_shield_wreath.svg",
     generalRank: 5
   },
+
   {
     name: "University of Cambridge",
-    logo: "https://upload.wikimedia.org/wikipedia/en/8/8a/University_of_Cambridge_coat_of_arms.svg",
     website: "https://www.cam.ac.uk",
+    logo: "https://upload.wikimedia.org/wikipedia/en/8/8a/University_of_Cambridge_coat_of_arms.svg",
     generalRank: 6
   },
+
   {
     name: "ETH Zurich",
-    logo: "https://upload.wikimedia.org/wikipedia/commons/f/f2/ETH_Zurich_Logo.svg",
     website: "https://ethz.ch",
+    logo: "https://upload.wikimedia.org/wikipedia/commons/f/f2/ETH_Zurich_Logo.svg",
     generalRank: 7
   },
+
   {
     name: "National University of Singapore",
-    logo: "https://upload.wikimedia.org/wikipedia/en/9/9a/NUS_coat_of_arms.svg",
     website: "https://www.nus.edu.sg",
+    logo: "https://upload.wikimedia.org/wikipedia/en/9/9a/NUS_coat_of_arms.svg",
     generalRank: 8
   },
+
   {
     name: "UCL",
-    logo: "https://upload.wikimedia.org/wikipedia/commons/0/04/UCL_logo.svg",
     website: "https://www.ucl.ac.uk",
+    logo: "https://upload.wikimedia.org/wikipedia/commons/0/04/UCL_logo.svg",
     generalRank: 9
   },
+
   {
     name: "California Institute of Technology",
-    logo: "https://upload.wikimedia.org/wikipedia/commons/7/75/Caltech_Logo.svg",
     website: "https://www.caltech.edu",
+    logo: "https://upload.wikimedia.org/wikipedia/commons/7/75/Caltech_Logo.svg",
     generalRank: 10
+  },
+
+  {
+    name: "University of Pennsylvania",
+    website: "https://www.upenn.edu",
+    logo: "https://upload.wikimedia.org/wikipedia/en/9/9a/University_of_Pennsylvania_Shield.svg",
+    generalRank: 11
+  },
+
+  {
+    name: "University of California, Berkeley",
+    website: "https://www.berkeley.edu",
+    logo: "https://upload.wikimedia.org/wikipedia/commons/1/1f/UC_Berkeley_Seal.svg",
+    generalRank: 12
   }
+
 ];
